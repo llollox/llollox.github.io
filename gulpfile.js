c516ac7c4b93f2ad6b8df1ecd93ef37612bf9651
llollox <lore91tanz@gmail.com>
2017-07-01 22:16:56 +0200

    Some path fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var pug         = require('gulp-pug');
 var stylus      = require('gulp-stylus');
+var concat      = require('gulp-concat');
+
+var paths       = {
+    'assets': './assets',
+    'bower' : './bower_components'
+};
 
 gulp.task('serve', [], function() {
 
@@ -16,10 +22,61 @@ gulp.task('serve', [], function() {
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
+gulp.task('fonts', function() {
+    return gulp.src([
+            paths.assets + '/fonts/**.*',
+            paths.bower  + '/font-awesome/fonts/**.*',
+            paths.bower  + '/bootstrap/fonts/**.*'
+        ])
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+            paths.bower  + '/jquery/dist/jquery.js',
+            paths.bower  + '/bootstrap/dist/js/bootstrap.js',
+            paths.bower  + '/modernizr/modernizr.js',
+            paths.bower  + '/waypoints/lib/jquery.waypoints.js',
+            paths.assets + '/js/**.js'
+        ])
+        .pipe(concat('application.js'))
+        .pipe(gulp.dest('./public/js'));
+});
+
+
+gulp.task('css_libraries', function () {
+    return gulp.src([
+        paths.bower  + '/bootstrap/dist/css/bootstrap.css',
+        paths.bower  + '/font-awesome/css/font-awesome.css',
+        paths.bower  + '/hover/css/hover.css',
+        paths.assets + '/css/**.css'
+    ])
+    .pipe(concat({ path: 'css_libraries.css'}))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('assets', function () {
+    return gulp.src([
+        paths.bower  + '/rs-plugin/assets/**.png'
+    ])
+        .pipe(gulp.dest('./public/assets'));
+});
+
+gulp.task('images', function () {
+    return gulp.src([
+            paths.bower   + '/rs-plugin/images/**/*.*',
+            paths.assets  + '/images/**/*.*'
+        ])
+        .pipe(gulp.dest('./public/images'));
+});
+
 gulp.task('styles', function () {
     return gulp.src('./stylus/main.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('./build/css'))
+        .pipe(stylus({
+            // compress: true
+        }))
+        .pipe(concat('application.css'))
+        .pipe(gulp.dest('./public/css'))
         .pipe(browserSync.stream());
 });
 
