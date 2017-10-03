539133912dfb5b67849c0e3092f3db3aead5dc3c
rigatol <rigatol@previnet.previdom.it>
2017-10-03 17:16:35 +0200

    Added some translations

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -1,33 +1,116 @@
 var dict = {
-  "Hi, I'm Lorenzo Rigato!"     : { it: "Lorenzo Rigato" },
-  "Web, Android & iOS Developer": { it: "Sviluppatore Web, Android e iOS" },
 
-  "About"           : { it: "Su di me"},
-  "Skills"          : { it: "Competenze" },
-  "Recent Projects" : { it: "Progetti Recenti" },
-  "Places"          : { it: "Luoghi"},
-  "Contacts"        : { it: "Contatti"},
+  // Banner ********************************************************************
+  "banner.title" : {
+    en: "Hi, I'm Lorenzo Rigato!",
+    it: "Lorenzo Rigato"
+  },
+  "banner.subtitle": {
+    en: "Web, Android & iOS Developer",
+    it: "Sviluppatore Web, Android e iOS"
+  },
 
-  "Android & iOS Developer" : { it: "Sviluppatore Android & iOS" },
-  "Web Developer"           : { it: "Sviluppatore Web" },
-  "Android Developer"       : { it: "Sviluppatore Android" },
-  "Ruby on Rails & Android Developer" : {it: "Sviluppatore Ruby on Rails & Android" },
 
-  "University of Trento"                  : {it: "Università di Trento" },
-  "110 cum laude"                         : {it: "110 e Lode"}
-  "Master's Degree in Computer Science"   : {it: "Laurea Magistrale in Informatica"},
-  "Bachelor's Degree in Computer Science" : {it: "Laurea Triennale in Informatica"}
-  "High School Diploma on Economy"        : {it: "Diploma di Ragioneria"},
 
-  "My Skills" : {it: "Le mie abilità" },
-  "Technical" : {it: "Tecniche" },
-  "Personal"  : {it: "Personali" },
+  // Navbar ********************************************************************
+  "navbar.about": {
+    en: "About",
+    it: "Su di me"
+  },
+  "navbar.skills": {
+    en: "Skills",
+    it: "Competenze"
+  },
+  "navbar.recent-projects": {
+    en: "Recent Projects",
+    it: "Progetti Recenti"
+  },
+  "navbar.places": {
+    en: "Places",
+    it: "Luoghi"
+  },
+  "navbar.contacts": {
+    en: "Contacts",
+    it: "Contatti"
+  },
 
-  "I've Lived" : {it: "Dove ho vissuto"},
 
-  "Send me a message":      {it: "Inviami un messaggio" },
-  "Get in touch with me!":  {it: "Mettiti in contatto con me!"},
-  "Below my accounts":      {it: "Di seguito i miei account"}
+
+  // Timeline ******************************************************************
+  "timeline.android-ios-dev" : {
+    en: "Android & iOS Developer",
+    it: "Sviluppatore Android & iOS"
+  },
+  "timeline.web-dev" : {
+    en: "Web Developer",
+    it: "Sviluppatore Web"
+  },
+  "timeline.ror-android-dev" : {
+    en: "Ruby on Rails & Android Developer",
+    it: "Sviluppatore Ruby on Rails & Android"
+  },
+  "timeline.university-trento": {
+    en: "University of Trento",
+    it: "Università di Trento"
+  },
+  "timeline.master-mark": {
+    en: "110 cum laude",
+    it: "110 e Lode"
+  },
+  "timeline.master-degree": {
+    en: "Master's Degree in Computer Science",
+    it: "Laurea Magistrale in Informatica"
+  },
+  "timeline.bachelor-degree": {
+    en: "Bachelor's Degree in Computer Science",
+    it: "Laurea Triennale in Informatica"
+  },
+  "timeline.high-school-diploma" : {
+    en: "High School Diploma on Economy",
+    it: "Diploma di Ragioneria"
+  },
+
+
+  // Certifications ************************************************************
+  "certifications.title": {
+    en: "Certifications",
+    it: "Certificazioni"
+  },
+
+  // Skills ********************************************************************
+  "skills.title": {
+    en: "My Skills",
+    it: "Le mie abilità"
+  },
+  "skills.technical": {
+    en: "Technical",
+    it: "Tecniche"
+  },
+  "skills.personal": {
+    en: "Personal",
+    it: "Personali"
+  },
+
+  // Places ********************************************************************
+  "places.lived": {
+    en: "I've Lived",
+    it: "Dove ho vissuto"
+  },
+
+
+  // Footer ********************************************************************
+  "footer.send-me-message": {
+    en: "Send me a message",
+    it: "Inviami un messaggio"
+  },
+  "footer.get-in-touch": {
+    en: "Get in touch with me!",
+    it: "Mettiti in contatto con me!"
+  },
+  "footer.accounts-title": {
+      en: "Below my accounts",
+      it: "Di seguito i miei account"
+  }
 }
 
 
