39cf5ba9cacdc076f3346da81814be152254940d
llollox <lore91tanz@gmail.com>
2017-10-06 21:59:39 +0200

    Refactoring references

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)};
+gtag('js', new Date());
+
+gtag('config', 'UA-88641355-2');
+
 /*!
  * jQuery JavaScript Library v1.12.2
  * http://jquery.com/
