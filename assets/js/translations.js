d68a23c669697a2a678308c7fc52e86864d301ae
llollox <lore91tanz@gmail.com>
2019-06-02 08:36:11 +0200

    - Aggiunto supporto internazionalizzazione. - Inizio traduzione in italiano - Aggiunto bower_components in gitignore

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -13,9 +13,13 @@ var dict = {
 
 
   // Navbar ********************************************************************
+  "navbar.home": {
+    en: "Home",
+    it: "Home"
+  },
   "navbar.about": {
-    en: "About",
-    it: "Su di me"
+    en: "About me",
+    it: "Chi sono"
   },
   "navbar.skills": {
     en: "Skills",
@@ -33,30 +37,145 @@ var dict = {
     en: "Contacts",
     it: "Contatti"
   },
+  "navbar.language": {
+    en: "Language",
+    it: "Lingua"
+  },
+  "navbar.english": {
+    en: "English",
+    it: "Inglese"
+  },
+  "navbar.italian": {
+    en: "Italian",
+    it: "Italiano"
+  },
 
 
 
   // Timeline ******************************************************************
-  "timeline.android-ios-dev" : {
+  "timeline.description": {
+    en: "Hello! I'm Lorenzo, a passionate 28 years old <strong>mobile and web developer</strong> with 5 years of experience.<br/> Strong knowledge on <strong>Android</strong> framework.<br/><strong>Clean code</strong> lover and passionate on <strong>software architectures</strong>.<br/>Able to complete projects satisfying customer’s <strong>requirements</strong>.<br/>Beautiful <strong>UI</strong> fascinated.",
+    it: "Ciao, mi chiamo Lorenzo! Ho 28 anni e sono un appassionato sviluppatore mobile e web con 5 anni di esperienza.<br/> Forte conoscienza del framework <strong>Android</strong>.<br/>Amante del <strong>codice pulito</strong> ed appassionato di <strong>architetture software</strong>. <br/>Capace di completare progetti soddisfando i requisiti del cliente.<br/>Affascinato da <strong>interfacce utente</strong> semplici ed efficaci"
+  },
+
+  // Previnet
+  "timeline.previnet.title" : {
     en: "Android & iOS Developer",
     it: "Sviluppatore Android & iOS"
   },
-  "timeline.web-dev" : {
+  "timeline.previnet.period" : {
+    en: "Nov 2016 - Present",
+    it: "Nov 2016 - Presente"
+  },
+  "timeline.previnet.description" : {
+    en: 
+      "<ul class='text-align-left'>" +
+        "<li><strong>Refactoring</strong> of two old internal Android applications following the Material design style guidelines.</li>" +
+        "<li>Development of an application for a bank, in both <strong>Android and iOS</strong> versions, to manage health services provided by a network of structures.</li>" +
+      "</ul>",
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li><strong>Refactoring</strong> of two old internal Android applications following the Material design style guidelines.</li>" +
+        "<li>Development of an application for a bank, in both <strong>Android and iOS</strong> versions, to manage health services provided by a network of structures.</li>" +
+      "</ul>"
+  },
+
+  // Reply
+  "timeline.reply.title" : {
     en: "Web Developer",
     it: "Sviluppatore Web"
   },
-  "timeline.ror-android-dev" : {
-    en: "Ruby on Rails & Android Developer",
+  "timeline.reply.period" : {
+    en: "Apr - Oct 2016",
+    it: "Apr - Ott 2016"
+  },
+  "timeline.reply.description" : {
+    en: 
+      "<ul class='text-align-left'>" +
+        "<li>Development of a <strong>responsive web application</strong> for an insurance company using Liferay</li>" +
+        "<li>Development of <strong>UI test suite</strong> using SAHI</li>" +
+      "</ul>",
+    it: "Descrizione lavoro Reply"
+  },
+
+  // Belka
+  "timeline.belka.title" : {
+    en: "Android Developer",
+    it: "Sviluppatore Android",
+  },
+  "timeline.belka.period" : {
+    en: "Sep 2015 - Mar 2016",
+    it: "Set 2015 - Mar 2016"
+  },
+  "timeline.belka.description" : {
+    en: 
+      "<ul class='text-align-left'>" +
+        "<li>Development of <strong>two Android applications</strong>. First on sharing events between friends and last on configuring electronic chips via NFC</li>" +
+        "<li>Improved skill on understanding customer's <strong>requirements</strong></li>" +
+      "</ul>",
+    it: "Descrizione lavoro Belka"
+  },
+
+  // FBK
+  "timeline.fbk.title" : {
+    en: "Intern Ruby on Rails & Android Developer",
+    it: "Intern Sviluppatore Ruby on Rails & Android"
+  },
+  "timeline.fbk.period" : {
+    en: "Dec 2011 - Sep 2015",
+    it: "Dic 2011 - Set 2015"
+  },
+  "timeline.fbk.description" : {
+    en: 
+      "<ul class='text-align-left'>" +
+        "<li><strong>Design</strong> and <strong>development</strong> of Ruby on Rails and Android applications against the food waste</li>" +
+        "<li><strong>Aggregation and organization</strong> from different sources of food information.</li>" +
+        "<li>Worked closely with <strong>senior developers</strong> and learned advanced programming and <strong>project management</strong> skills</li>" +
+      "</ul>",
     it: "Sviluppatore Ruby on Rails & Android"
   },
-  "timeline.university-trento": {
-    en: "University of Trento",
-    it: "Università di Trento"
+
+  // Msc UniTn
+  "timeline.unitn.msc.title" : {
+    en: "Master's Degree in Computer Science",
+    it: "Laurea Magistrale in Informatica"
   },
-  "timeline.master-mark": {
+  "timeline.unitn.msc.period" : {
+    en: "Sep 2013 - Dec 2015",
+    it: "Set 2013 - Dic 2015"
+  },
+  "timeline.unitn.msc.mark" : {
     en: "110 cum laude",
     it: "110 e Lode"
   },
+  "timeline.unitn.msc.description" : {
+    en: 
+      "Thesis: '<strong>Quanto Spreco?</strong>'. " +
+      "An application to monitor and reduce the domestic food waste",
+    it: "Descrizione laurea magistrale"
+  },
+
+  // Bsc UniTn
+  "timeline.unitn.bsc.title" : {
+    en: "Bachelor's Degree in Computer Science",
+    it: "Laurea Triennale in Informatica"
+  },
+  "timeline.unitn.bsc.period" : {
+    en: "Sep 2010 - Sep 2013",
+    it: "Set 2010 - Set 2013"
+  },
+  "timeline.unitn.bsc.description" : {
+    en: 
+      "Discovered a strong interest on networking. <br/>" +
+      "Thesis: '<strong>Bring the *'</strong>. An extension to the Bring the Food application to support any kind of donation.",
+    it: "Descrizione laurea triennale"
+  },
+
+  "timeline.unitn": {
+    en: "University of Trento",
+    it: "Università di Trento"
+  },
+
   "timeline.master-degree": {
     en: "Master's Degree in Computer Science",
     it: "Laurea Magistrale in Informatica"
@@ -116,3 +235,7 @@ var dict = {
 
 
 var translator = $('body').translate({lang: "en", t: dict});
+
+function changeLanguage(language) {
+  translator.lang(language);
+}
