c61ab02914d278f71c0146a8a448683359887a1a
llollox <lore91tanz@gmail.com>
2019-06-02 15:42:03 +0200

    - Fix css progress bar - Fix feedback people - Fix immagini feedbacks

--- public/js/application.js
+++ public/js/application.js
@@ -28451,14 +28451,14 @@ var dict = {
   // Timeline ******************************************************************
   "timeline.description": {
     en: 
-      "Hello! I'm Lorenzo, nice to meet you!" + 
+      "Hello! I'm Lorenzo, nice to meet you!<br/>" + 
       "I'm a 28 years old <strong>mobile and web developer</strong> with 5 years of experience.<br/>" + 
       "Strong knowledge on <strong>Android</strong> framework.<br/>" +
       "Clean code and <strong>software architectures</strong> lover.<br/>" + 
       "Simple and clear <strong>UI</strong> amazed.",
     
     it: 
-      "Ciao! Mi chiamo Lorenzo, piacere di conoscerti!" + 
+      "Ciao! Mi chiamo Lorenzo, piacere di conoscerti!<br/>" + 
       "Ho 28 anni e sono uno <strong>sviluppatore mobile e web</strong> con 5 anni di esperienza.<br/>" + 
       "Grande conoscienza del framework <strong>Android</strong>.<br/>" + 
       "Amante del <strong>codice pulito</strong> e delle <strong>architetture software</strong>.<br/>" + 
@@ -28533,10 +28533,12 @@ var dict = {
   "timeline.reply.description" : {
     en: 
       "<ul class='text-align-left'>" +
-        "<li>Development of a <strong>responsive web application</strong> for an insurance company using Liferay</li>" +
-        "<li>Development of <strong>UI test suite</strong> using SAHI</li>" +
+        "<li>Development of the Direct Line's responsive web portal using <strong>Bootstrap, jQuery</strong> and <strong>Liferay</strong>.</li>" +
       "</ul>",
-    it: "Descrizione lavoro Reply"
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li>Sviluppo del portale web responsive per Direct Line's utilizzando <strong>Bootstrap, jQuery</strong> e <strong>Liferay</strong>.</li>" +
+      "</ul>",
   },
 
   // Belka
@@ -28551,16 +28553,26 @@ var dict = {
   "timeline.belka.description" : {
     en: 
       "<ul class='text-align-left'>" +
-        "<li>Development of <strong>two Android applications</strong>. First on sharing events between friends and last on configuring electronic chips via NFC</li>" +
-        "<li>Improved skill on understanding customer's <strong>requirements</strong></li>" +
+        "<li>Implementation of Android interface following <strong>Material Design</strong> rules." +
+        "<li>Android integration with electronic chips using NFC.</li>" +
+        "<li>Implementation of UI tests using <strong>Roboelectric</strong>.</li>" +
+        "<li>Implementation of <strong>Toggle Switch Button</strong> library.</li>" +
+        "<li>Usage of <strong>Jira</strong> and <strong>Kanban board</strong> to organize tasks within the team.</li>" +
       "</ul>",
-    it: "Descrizione lavoro Belka"
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li>Implementazione di un'interfaccia Android seguendo <strong>Material Design</strong>" +
+        "<li>Integrazione di Android con dei chip elettronici usando NFC.</li>" +
+        "<li>Implementazione di test della UI utilizzando <strong>Roboelectric</strong>.</li>" +
+        "<li>Implementazione della libreria <strong>Toggle Switch Button</strong>.</li>" +
+        "<li>Utilizzo di <strong>Jira</strong> e della <strong>Kanban board</strong> per organizzare il lavoro nel team.</li>" +
+      "</ul>"
   },
 
   // FBK
   "timeline.fbk.title" : {
-    en: "Intern Ruby on Rails & Android Developer",
-    it: "Intern Sviluppatore Ruby on Rails & Android"
+    en: "Internship and assistant Ruby on Rails & Android Developer",
+    it: "Tirocinante ed assistente Sviluppatore Ruby on Rails & Android"
   },
   "timeline.fbk.period" : {
     en: "Dec 2011 - Sep 2015",
@@ -28569,11 +28581,22 @@ var dict = {
   "timeline.fbk.description" : {
     en: 
       "<ul class='text-align-left'>" +
-        "<li><strong>Design</strong> and <strong>development</strong> of Ruby on Rails and Android applications against the food waste</li>" +
-        "<li><strong>Aggregation and organization</strong> from different sources of food information.</li>" +
-        "<li>Worked closely with <strong>senior developers</strong> and learned advanced programming and <strong>project management</strong> skills</li>" +
+        "<li>Development of <strong>Android applications</strong> with backward compatibility with Android 2.</li>" +
+        "<li>Development of the frontend web using <strong>Bootstrap</strong></li>" +
+        "<li>Implementation of versioned <strong>REST API</strong> on Ruby on Rails</li>" +
+        "<li>Implementation of <strong>Unit and UI tests</strong> using Rspec, Capybara, Selenium and Cucumber.</li>" +
+        "<li>Written code <strong>documentation</strong> for API and project structure.</li>" +
+        "<li>Usage of <strong>git</strong> as version control." +
       "</ul>",
-    it: "Sviluppatore Ruby on Rails & Android"
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li>Sviluppo di <strong>applicazioni Android</strong> retrocompatibili con Android 2.</li>" +
+        "<li>Sviluppo di frontend web utilizzando <strong>Bootstrap</strong></li>" +
+        "<li>Implementazione di <strong>REST API</strong> versionate in Ruby on Rails</li>" +
+        "<li>Implementazione di <strong>Unit e UI test</strong> utilizzando Rspec, Capybara, Selenium and Cucumber.</li>" +
+        "<li>Stesura della <strong>documentation</strong> del codice per API e struttura dei progetti.</li>" +
+        "<li>Utilizzo di <strong>git</strong> come strumento per il versionamento del codice." +
+      "</ul>",
   },
 
   // Msc UniTn
@@ -28591,9 +28614,17 @@ var dict = {
   },
   "timeline.unitn.msc.description" : {
     en: 
-      "Thesis: '<strong>Quanto Spreco?</strong>'. " +
-      "An application to monitor and reduce the domestic food waste",
-    it: "Descrizione laurea magistrale"
+      "<ul class='text-align-left'>" +
+        "<li>Thesis: '<strong>Quanto Spreco?</strong>'. An application to monitor and reduce the domestic food waste</li>" +
+        "<li>Development of an update for the OpenFWWF firmware in <strong>Assembly</strong> to allow the access point and its clients to change channel together. " +
+        "Also, development of a <strong>Python</strong> controller to gather metrics information and decide which is the best channel.</li>" +
+      "</ul>",
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li>Thesis: '<strong>Quanto Spreco?</strong>'. Un'applicazione per monitorare e ridurre lo spreco alimentare domestico.</li>" +
+        "<li>Sviluppo di un aggiornamento del firmware OpenFWWF in <strong>Assembly</strong> per permettere all'access point, ed ai client collegati, di cambiare canale. " +
+        "Inoltre, sviluppo di un controller in <strong>Python</strong> per ottenere le informazioni dell'andamento della rete e calcolare il miglior canale in cui spostarsi.</li>" +
+      "</ul>",
   },
 
   // Bsc UniTn
@@ -28606,10 +28637,16 @@ var dict = {
     it: "Set 2010 - Set 2013"
   },
   "timeline.unitn.bsc.description" : {
-    en: 
-      "Discovered a strong interest on networking. <br/>" +
-      "Thesis: '<strong>Bring the *'</strong>. An extension to the Bring the Food application to support any kind of donation.",
-    it: "Descrizione laurea triennale"
+    en:
+      "<ul class='text-align-left'>" +
+        "<li>Thesis: '<strong>Bring the *'</strong>. An extension to the Bring the Food application to support any kind of donation.</li>" +
+        "<li>Discovered interest on <strong>networking</strong>.</li>" +
+      "</ul>",
+    it: 
+      "<ul class='text-align-left'>" +
+        "<li>Thesis: '<strong>Bring the *'</strong>. An extension to the Bring the Food application to support any kind of donation.</li>" +
+        "<li>Scoperto un interesse nel funzionamento delle <strong>reti di computer</strong>.</li>" +
+      "</ul>"
   },
 
   "timeline.unitn": {
@@ -28630,6 +28667,56 @@ var dict = {
     it: "Diploma di Ragioneria"
   },
 
+  // References ****************************************************************
+  "references.title": {
+    en: "People I worked with",
+    it: "Hanno lavorato con me"
+  },
+
+  "references.belka" : {
+    en: 
+      "Lorenzo is an able and independent developer. " +
+      "In my experience he accomplished his job autonomously " +
+      "providing also some improvements on the user interface.",
+
+    it: 
+      "Lorenzo è uno sviluppatore abile ed indipendente." +
+      "Nella mia esperienza è riuscito a compiere i lavori assegnati in autonomia," +
+      "fornendo alcuni miglioramenti anche all'interfaccia utente."
+  },
+
+  "references.wrb" : {
+    en: 
+      "We have known Lorenzo since he was sixteen years old, when he came to ask suggestions in order to increase speed of his motorbike." +
+      "In 2012, he was studing computer science, we asked to Lorenzo to develop the new website of our company." +
+      "He started immediately with this new project and then he also proposed new features that we have never thought." +
+      "We saw him work very hard, even late at night, in order to build our web application" +
+      "Very few times, we saw people that able to work with passion like Lorenzo." +
+      "Thank you very much Lorenzo, for everything you did for us." +
+      "We wish you a good future for you and your career. You deserve it!",
+
+    it: 
+      "Lorenzo lo conosciamo da quando sedicenne veniva a chiedere suggerimenti e consigli per elaborare la sua moto, trucchi e modifiche che aveva scovato su internet, e che avrebbe voluto mettere in pratica sulla sua moto." +
+      "Seguito negli anni abbiamo saputo che aveva intrapreso gli studi in Ingegneria informatica." +
+      "Chi chiedere se non a lui che ha 2 grandi passioni, i motori e l’informatica, quando abbiamo avuto bisogno del nostro nuovo sito aziendale!" +
+      "Da subito, si è coinvolto nel nuovo progetto proponendo argomenti, soluzioni ed idee per la messa in rete del nuovo sito che a noi non erano nemmeno venute in mente, pur se più di lui esperti del nostro settore." +
+      "Lo abbiamo visto dedicarsi anima e corpo nella progettazione e nello sviluppo del nostro sito, usando qui poche spazi di tempo tolti allo studio, anche lavorando fino ad orari improponibili. " +
+      "Che dire, poche volte abbiamo visto persone molto giovani che hanno tanta passione per quello che fanno e che vogliono realizzare." +
+      "Grazie Lorenzo, per quello che hai realizzato e per come sei anche come persona." +
+      "Ti auguriamo un proseguimento della tua attività lavorativa nel migliore dei modi e per te con maggior soddisfazione possibile, lo meriti!"
+  },
+
+  "references.foodracers" : {
+    en: 
+      "Lorenzo è una persona capace e indipendente. " +
+      "Riesce ad imparare velocemente nuove tecnologie e sviluppare in totale autonomia un progetto completo e complesso. " +
+      "È stato un piacere lavorare con lui per questo è assolutamente raccomandato.",
+
+    it: 
+      "Lorenzo è una persona capace e indipendente. " +
+      "Riesce ad imparare velocemente nuove tecnologie e sviluppare in totale autonomia un progetto completo e complesso. " +
+      "È stato un piacere lavorare con lui per questo è assolutamente raccomandato."
+  },
 
   // Certifications ************************************************************
   "certifications.title": {
